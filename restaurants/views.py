4aaa65e0ab6cfbddae95d69b9679d6478ab1c9ba
hamadk24 <hamadalkhaled24@gmail.com>
2020-01-12 19:30:35 +0300

    task2

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hello_world(request):
+	context = {
+		"msg": "Hello World!",
+		}
+	return render(request, 'html.html', context)# Create your views here.
